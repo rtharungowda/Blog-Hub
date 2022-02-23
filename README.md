c812799c4ff37645a99b30b7eb989947d13b7d33
rtharungowda <rtharun.gowda.cd.ece19@itbhu.ac.in>
2022-02-23 15:52:36 +0530

    add forms

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Blog-Hub
 Hosting blogs using flask
+
+## Install dependencies
+```
+cd Blog-Hub
+pip3 install -r requriments.txt
+```
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 posts = [
@@ -24,6 +25,16 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title="About")
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title="Sign Up", form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Sign Up", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
